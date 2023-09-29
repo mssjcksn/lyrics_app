0a981bb4ae37813669654d8a613495ba1dd30f6c
Dawn <dydawnraine@gmail.com>
2023-09-29 23:30:56 +0800

    works as intended. could be written better

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import { Container, TextField } from "@mui/material/";
 import { Route, Routes, Link } from "react-router-dom";
-import React from "react";
+import React, { useState } from "react";
 import SingerPage from "./SingerPage";
 import FirstSinger from "./FirstSinger";
 import SecondSinger from "./SecondSinger";
@@ -9,6 +9,26 @@ import FourthSinger from "./FourthSinger";
 import "./App.css";
 
 function App() {
+  const [input, setInput] = useState("");
+  const [lyrics, setLyrics] = useState([]);
+  const [singer, setSinger] = useState(-1);
+  const singerColors = ["#E0B0FF", "#74C365", "#29AB87", "#FF91AF"];
+
+  const handleInputChange = (e) => {
+    setInput(e.target.value);
+  };
+
+  const handleSingerChange = (newSinger) => {
+    if (input.trim() !== "") {
+      setLyrics((prevLyrics) => [
+        { text: input, singer: singer !== null ? singer : newSinger },
+        ...prevLyrics,
+      ]);
+      setInput("");
+    }
+    setSinger(newSinger);
+  };
+
   return (
     <>
       <Container
@@ -25,25 +45,41 @@ function App() {
         </div>
 
         <div className="singer-row">
-          <Link to="/singers/first" style={{ textDecoration: "none" }}>
+          <Link
+            to="/singers/first"
+            style={{ textDecoration: "none" }}
+            onClick={() => handleSingerChange(0)}
+          >
             <div className="singer-box" style={{ backgroundColor: "#E0B0FF" }}>
               FIRST SINGER
             </div>
           </Link>
 
-          <Link to="/singers/second" style={{ textDecoration: "none" }}>
+          <Link
+            to="/singers/second"
+            style={{ textDecoration: "none" }}
+            onClick={() => handleSingerChange(1)}
+          >
             <div className="singer-box" style={{ backgroundColor: "#74C365" }}>
               SECOND SINGER
             </div>
           </Link>
 
-          <Link to="/singers/third" style={{ textDecoration: "none" }}>
+          <Link
+            to="/singers/third"
+            style={{ textDecoration: "none" }}
+            onClick={() => handleSingerChange(2)}
+          >
             <div className="singer-box" style={{ backgroundColor: "#29AB87" }}>
               THIRD SINGER
             </div>
           </Link>
 
-          <Link to="/singers/fourth" style={{ textDecoration: "none" }}>
+          <Link
+            to="/singers/fourth"
+            style={{ textDecoration: "none" }}
+            onClick={() => handleSingerChange(3)}
+          >
             <div className="singer-box" style={{ backgroundColor: "#FF91AF" }}>
               FOURTH SINGER
             </div>
@@ -57,21 +93,51 @@ function App() {
           </Routes>
         </div>
 
-        <TextField
-          id="outlined-basic"
-          variant="outlined"
-          InputProps={{
-            style: {
-              width: "700px",
-              marginTop: "15px",
-              marginBottom: "15px",
-              borderRadius: "15px",
-              border: "1px solid #6CB4EE",
-            },
-          }}
-        />
+        {singer !== null && (
+          <TextField
+            id="outlined-basic"
+            variant="outlined"
+            InputProps={{
+              style: {
+                width: "700px",
+                marginTop: "15px",
+                marginBottom: "15px",
+                borderRadius: "15px",
+                border: "1px solid #6CB4EE",
+              },
+            }}
+            value={input}
+            onChange={handleInputChange}
+          />
+        )}
 
-        <div className="lyrics-box"></div>
+        <div className="lyrics-box">
+          <div>
+            {lyrics
+              .slice()
+              .reverse()
+              .map((lyric, index) => (
+                <div
+                  className="lyric"
+                  key={index}
+                  style={{ backgroundColor: singerColors[lyric.singer] }}
+                >
+                  {lyric.text}
+                </div>
+              ))}
+          </div>
+          {input.trim() !== "" && (
+            <div
+              className="lyric"
+              style={{
+                margin: "1px",
+                backgroundColor: singer !== null ? singerColors[singer] : "",
+              }}
+            >
+              {input}
+            </div>
+          )}
+        </div>
       </Container>
     </>
   );
